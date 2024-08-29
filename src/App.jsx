cb4109fd6207a80e47dbfdbce90497c52c2f2bf7
Sakshi <sakshi35sin@gmail.com>
2024-08-29 22:07:30 +0530

    Initialize the Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+  return (
+    <>
+      <h1>Blog Website</h1>
+    </>
+  )
+}
+
+export default App
