9302f41140f41a638e083b016b9d576aaffac5de
Sakshi <sakshi35sin@gmail.com>
2024-08-30 10:47:24 +0530

    Configure Tailwind and conditional Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from "./store/authSlice"
+
+
 
 function App() {
   
-  console.log(import.meta.env.REACT_APP_APPWRITE_URL);
-  return (
-    <>
-      <h1>Blog Website</h1>
-    </>
-  )
+  const [ loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>
+    console.log(error)
+    )
+    .finally(()=>setLoading(false))
+  },[])
+  
+  return !loading ? (
+    <div className=''></div>
+  ) : null
 }
 
 export default App
