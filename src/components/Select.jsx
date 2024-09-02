754882552025f067e9efa40d941a6756bcdfec06
Sakshi <sakshi35sin@gmail.com>
2024-09-02 11:56:56 +0530

    Signup and Login Form Updated

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -13,10 +13,13 @@ function Select({
            {
             label && <label htmlFor= {id} 
             className="" ></label>}
-            <select {...props} id= {id} ref={ref} 
+            <select {...props} 
+            id= {id} 
+            ref={ref} 
             className= {` px-3 py-2 rounded-lg bg-white
             text-black outline-none focus:bg-gray-50
-            duration-200 border border-gray-200 w-full ${className}`}>
+            duration-200 border border-gray-200 w-full ${className}`}
+            >
                 {options?.map((option)=>(
                     <option key = {option} value={option}>
                         {option}
--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import Footer from "./Footer/Footer.jsx"
 import Container from './container/Container.jsx'
 import Logo from "./Logo.jsx"
 import LogoutBtn from "./Header/LogoutBtn.jsx"
+import Button from './Button.jsx'
+import Input from "./Input.jsx"
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input
 }
